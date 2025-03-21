6e32a3a589e370cd09c9ac1cbb4c6f5f2e816548
igl-myrick <igl.myrick@gmail.com>
2025-03-21 15:45:54 -0700

    add Login route

--- PierresTreats/Controllers/AccountController.cs
+++ PierresTreats/Controllers/AccountController.cs
@@ -23,7 +23,7 @@ namespace PierresTreats.Controllers
     {
       return View();
     }
-    
+
     public IActionResult Register()
     {
       return View();
@@ -50,6 +50,33 @@ namespace PierresTreats.Controllers
           {
             ModelState.AddModelError("", error.Description);
           }
+          return View(model);
+        }
+      }
+    }
+
+     public ActionResult Login()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Login(LoginViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          ModelState.AddModelError("", "There is something wrong with your email or username. Please try again.");
           return View(model);
         }
       }
