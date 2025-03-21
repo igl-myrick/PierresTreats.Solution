b3b5f2a79fbc41a807198f998a3dcfa03b5f7f3c
igl-myrick <igl.myrick@gmail.com>
2025-03-21 15:38:18 -0700

    add Register route

--- PierresTreats/Controllers/AccountController.cs
+++ PierresTreats/Controllers/AccountController.cs
@@ -23,5 +23,36 @@ namespace PierresTreats.Controllers
     {
       return View();
     }
+    
+    public IActionResult Register()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Register (RegisterViewModel model)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(model);
+      }
+      else
+      {
+        ApplicationUser user = new ApplicationUser { UserName = model.Email };
+        IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+        if (result.Succeeded)
+        {
+          return RedirectToAction("Index");
+        }
+        else
+        {
+          foreach (IdentityError error in result.Errors)
+          {
+            ModelState.AddModelError("", error.Description);
+          }
+          return View(model);
+        }
+      }
+    }
   }
 }
