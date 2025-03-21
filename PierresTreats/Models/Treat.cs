6f10d28daf91677bb4b19d1a8026ce923ba3d8aa
igl-myrick <igl.myrick@gmail.com>
2025-03-21 13:56:56 -0700

    add Treat class

---
+++ PierresTreats/Models/Treat.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PierresTreats.Models
+{
+  public class Treat
+  {
+    public int TreatId { get; set; }
+    [Required(ErrorMessage = "Please enter a valid name.")]
+    public string Name { get; set; }
+    public List<FlavorTreat> JoinEntities { get; }
+    public ApplicationUser User { get; set; }
+  }
+}
