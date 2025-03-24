6012c6f4a2f6b6fde3d9acab531860486fab75cb
igl-myrick <igl.myrick@gmail.com>
2025-03-24 14:06:58 -0700

    remove check for signed in users

--- PierresTreats/Controllers/HomeController.cs
+++ PierresTreats/Controllers/HomeController.cs
@@ -2,37 +2,25 @@ using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 using System.Linq;
 using PierresTreats.Models;
-using Microsoft.AspNetCore.Identity;
-using System.Threading.Tasks;
-using System.Security.Claims;
 
 namespace PierresTreats.Controllers
 {
   public class HomeController : Controller
   {
     private readonly PierresTreatsContext _db;
-    private readonly UserManager<ApplicationUser> _userManager;
 
-    public HomeController(UserManager<ApplicationUser> userManager, PierresTreatsContext db)
+    public HomeController(PierresTreatsContext db)
     {
-      _userManager = userManager;
       _db = db;
     }
 
-    public async Task<ActionResult> Index()
+    public ActionResult Index()
     {
       Flavor[] flavors = _db.Flavors.ToArray();
+      Treat[] treats = _db.Treats.ToArray();
       Dictionary<string, object[]> model = new Dictionary<string, object[]>();
       model.Add("flavors", flavors);
-      string userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
-      ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
-      if (currentUser != null)
-      {
-        Treat[] treats = _db.Treats
-          .Where(entry => entry. User.Id == currentUser.Id)
-          .ToArray();
-        model.Add("treats", treats);
-      }
+      model.Add("treats", treats);
       return View(model);
     }
   }
