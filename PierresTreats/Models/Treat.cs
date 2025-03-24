4ccf7bebd580d8abccd7d44a33846ee0cb6d141a
igl-myrick <igl.myrick@gmail.com>
2025-03-24 14:10:14 -0700

    remove ApplicationUser property

--- PierresTreats/Models/Treat.cs
+++ PierresTreats/Models/Treat.cs
@@ -9,6 +9,5 @@ namespace PierresTreats.Models
     [Required(ErrorMessage = "Please enter a valid name.")]
     public string Name { get; set; }
     public List<FlavorTreat> JoinEntities { get; }
-    public ApplicationUser User { get; set; }
   }
 }
