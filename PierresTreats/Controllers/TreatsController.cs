316e738883172dc45cc5d8467a5abe655daa8a0a
igl-myrick <igl.myrick@gmail.com>
2025-03-24 14:39:48 -0700

    add Details route

--- PierresTreats/Controllers/TreatsController.cs
+++ PierresTreats/Controllers/TreatsController.cs
@@ -46,5 +46,14 @@ namespace PierresTreats.Controllers
         return RedirectToAction("Index");
       }
     }
+
+    public ActionResult Details(int id)
+    {
+      Treat thisTreat = _db.Treats
+        .Include(treat => treat.JoinEntities)
+        .ThenInclude(join => join.Flavor)
+        .FirstOrDefault(treat => treat.TreatId == id);
+      return View(thisTreat);
+    }
   }
 }
