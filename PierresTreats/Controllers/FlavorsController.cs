8195df07bce35ec351a0d145fa71ad52bb38a556
igl-myrick <igl.myrick@gmail.com>
2025-03-25 14:24:15 -0700

    add Delete route

--- PierresTreats/Controllers/FlavorsController.cs
+++ PierresTreats/Controllers/FlavorsController.cs
@@ -88,5 +88,20 @@ namespace PierresTreats.Controllers
       _db.SaveChanges();
       return RedirectToAction("Index");
     }
+
+    public ActionResult Delete(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      return View(thisFlavor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Flavor thisFlavor = _db.Flavors.FirstOrDefault(flavor => flavor.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
   }
 }
